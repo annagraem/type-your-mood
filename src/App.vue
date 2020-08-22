417c90706cc9344e69c9ba176ce879ed4435003a
annagraem <4anya.alstreim@gmail.com>
2020-08-22 16:18:01 +0200

    Make first functioning prototype

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
-  <div id="app">
+  <div id="app" v-bind:style="appStyle">
     <input v-model="userInput" placeholder="How are you feeling today?">
-    <p>[Debug] mood = {{ mood }}</p>
   </div>
 </template>
 
@@ -13,23 +12,77 @@ export default {
   data: function() {
     return {
       userInput: '',
+      leftBadMood: [106,133,182],
+      rightBadMood: [186,200,224],
+      leftGoodMood: [250,112,154],
+      rightGoodMood: [254,225,64],
     }
   },
   computed: {
-    mood: function () {
-      return vader.SentimentIntensityAnalyzer.polarity_scores(this.userInput).compound;
+    appStyle: function () {
+      let mood = vader.SentimentIntensityAnalyzer.polarity_scores(this.userInput).compound;
+      mood = (mood + 1) / 2;
+      console.log("mood (between 0 and 1) : ", mood);
+      let leftColor = [];
+      for (let i = 0; i < 3; i++) {
+        leftColor[i] = Math.round((1 - mood) * this.leftBadMood[i] + mood * this.leftGoodMood[i]);
+      }
+      let rightColor = [];
+      for (let i = 0; i < 3; i++) {
+        rightColor[i] = Math.round((1 - mood) * this.rightBadMood[i] + mood * this.rightGoodMood[i]);
+      }
+
+      return { backgroundImage: `linear-gradient(to right, rgb(${leftColor[0]}, ${leftColor[1]}, ${leftColor[2]}) 0%, rgb(${rightColor[0]}, ${rightColor[1]}, ${rightColor[2]}) 100%)` }
     }
   }
 }
 </script>
 
 <style lang="scss">
+:root {
+  --text-color: white;
+  --placeholder-color: rgba(255, 255, 255, 0.5);
+  --border-color: rgba(255, 255, 255, 0.2);
+}
+
+html, body {
+    height: 100%;
+    margin: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-family: sans-serif;
+  color: var(--text-color);
+  background-color: #c3cfe2;
+}
+
+input {
+  font-size: 3em;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: none;
+  border: none;
+  color: inherit;
+  border-bottom: 3px solid var(--border-color);
+
+  &:focus {
+    border: none;
+    outline: none;
+    border-bottom: 3px solid var(--text-color);
+  }
+}
+
+::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+  color: var(--placeholder-color);
+  opacity: 1; /* Firefox */
+}
+
+::-ms-input-placeholder { /* Microsoft Edge */
+  color: var(--placeholder-color);
 }
 </style>
